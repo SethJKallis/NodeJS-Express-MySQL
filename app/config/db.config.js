fcdf68450502e75ec557583ae83cdb030eb0deee
SethJKallis <kallisseth@gmail.com>
2023-02-14 13:33:04 +0200

    nodejs restful api with express and mysql

---
+++ app/config/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    HOST: "buionaiwv6zmwczcjobx-mysql.services.clever-cloud.com",
+    USER: "u9gxhqzgpmwvq3up",
+    PASSWORD: "Tm22wmfjOYSMynojQLUn",
+    DB: "buionaiwv6zmwczcjobx"
+};
---
+++ app/routes/tutorial.routes.js
@@ -0,0 +1,27 @@
+module.exports = app => {
+    const tutorials = require("../controllers/tutorial.controller.js");
+
+    let router = require("express").Router();
+
+    //Create a new tutorial
+    router.post('/', tutorials.create);
+
+    //Retrieve all tutorials
+    router.get('/', tutorials.findAll);
+
+    //Retrieve all published tutorials
+    router.get('/published', tutorials.findAllPublished);
+
+    //Retrieve a single tutorial
+    router.get('/:id', tutorials.findOne);
+
+    //Update a tutorial with id
+    router.put('/:id', tutorials.update);
+
+    //Delete a tutorial
+    router.delete('/:id', tutorials.delete);
+
+    router.delete('/', tutorials.deleteAll);
+
+    app.use('/api/tutorials', router);
+};
